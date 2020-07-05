4749b170ea67395caaea73c0bad82db98d2f0654
Marcelo Monnerat Castello <48702825+MonneratRJ@users.noreply.github.com>
2020-07-05 18:18:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var loop = setInterval(function(){ document.querySelector('[aria-label="Curti"]').click(); }, 666);
+// Check what aria-label your languate button has, in PT-BR is "Curti"... so we used that. Use F12, devtools inspector to do so.
+var button = document.querySelector('[aria-label="Curti"]');
 
+// Function to start the loop with 666 miliseconds time (you can change if you want)
+var loop = setInterval(function(){ button.click(); }, 666);
+
+// Use this command to stop the function.
 clearInterval(loop);
